1c103194cf47f80342ea9fa879bfeef67cce18da
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-21 14:42:35 +0200

    Exercise six

---
+++ homework/01/e06.py
@@ -0,0 +1,10 @@
+print("anna luku 1")
+luku1 = int(input())
+print("anna luku 2")
+luku2 = int(input())
+print("anna luku 3")
+luku3 = int(input())
+
+keskiarvo = (luku1 + luku2 + luku3)/3
+
+print(keskiarvo)
