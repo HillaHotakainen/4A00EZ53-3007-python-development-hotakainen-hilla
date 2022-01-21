2d6215fab28fafd180620d0f51ab69ae7db15915
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-21 14:03:55 +0200

    ensimmainen tehtava

--- homework/01/e01.py
+++ homework/01/e01.py
@@ -1,1 +1,22 @@
-print("This is exercise 01")
+# Luodaan kilpikonna
+import turtle
+kilpikonna = turtle.Turtle()
+kilpikonna.shape("turtle")
+
+# Mene eteenpäin 50 pikseliä
+kilpikonna.forward(50)
+
+# Käänny vasemmalle 90 astetta
+kilpikonna.left(90)
+
+# Mene eteenpäin 50 pikseliä
+kilpikonna.forward(50)
+
+# Käänny vasemmalle 90 astetta
+kilpikonna.left(90)
+kilpikonna.forward(50)
+kilpikonna.left(90)
+kilpikonna.forward(50)
+
+# Toteuta tänne toiminnallisuus jossa kilpikonna piirtää neliön
+# ja palaa alkuperäiseen pisteeseen.
