2c0879d35b1aafeb8db472fd0cc79a11e99c5f4e
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-21 14:29:54 +0200

    fourth exercise

---
+++ homework/01/e04.py
@@ -0,0 +1,20 @@
+# Tulosta tietokoneen näytölle teksti "Anna luku 1".
+print("Anna luku 1")
+
+# Ota käyttäjältä luku 1 vastaan ja talleta se muuttujaan luku1.
+luku1 = int( input() )
+
+# Tulosta tietokoneen näytölle teksti "Anna luku 2".
+print("Anna luku 2")
+
+# Ota käyttäjältä luku 1 vastaan ja talleta se muuttujaan luku2.
+luku2 = int( input() )
+
+# Laske luku1 * luku2 ja tallenna tulos muuttujaan summa.
+summa = luku1 * luku2
+
+# Tulosta tietokoneen näytölle teksti "Summa = "
+print("Summa = ")
+
+# Tulosta tietokoneen näytölle summa muuttujan sisältö.
+print(summa)
