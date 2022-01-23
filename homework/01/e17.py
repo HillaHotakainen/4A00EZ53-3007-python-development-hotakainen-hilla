e5fa6e486c8089ec1ff4966fd3a18087b5c33534
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 11:58:49 +0200

    I may have skipped 17, so here's 17 and 18

--- homework/01/e17.py
+++ homework/01/e17.py
@@ -1,6 +1,12 @@
-print("Anna luku")
-luku = int(input())
-i = 0
-while i < luku:
-    print("Terve maailma")
-    i = i + 1
+import random
+
+salainen_luku = random.randint(0, 10)
+kayttajan_syote = -1
+arvaukset = 0
+while kayttajan_syote != salainen_luku:
+    print("Arvaa salainen luku (0 - 10)")
+    kayttajan_syote = int( input() )
+    arvaukset = arvaukset + 1
+
+print("oikein!")
+print("Arvasit " + str(arvaukset) + " Kertaa")
