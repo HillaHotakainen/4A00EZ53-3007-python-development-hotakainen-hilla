9a4ff116fc97ca1e57fee50efe8b77d20da4c455
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 11:41:14 +0200

    Exercise 15

---
+++ homework/01/e15.py
@@ -0,0 +1,8 @@
+print("Anna kuukausi")
+kuukausi = int(input())
+print("Anna päivä")
+päivä = int(input())
+if kuukausi == 5 and päivä == 1 or kuukausi == 12 and päivä == 6:
+    print("nyt on vappu tai itsenäisyyspäivä")
+else:
+    print("Nyt ei ole vappu tai itsenäisyyspäivä :c")
