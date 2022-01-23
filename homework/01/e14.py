60a7bd166cb813ad4b60f25425a68093b151cfe0
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 11:37:10 +0200

    Exercise 14

---
+++ homework/01/e14.py
@@ -0,0 +1,8 @@
+print("Anna kuukausi")
+kuukausi = int(input())
+print("Anna päivä")
+päivä = int(input())
+if kuukausi == 12 and päivä == 24:
+    print("Hyvää joulua")
+else:
+    print("Nyt ei ole vielä joulu")
