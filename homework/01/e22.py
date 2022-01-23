69ae8ef3ad0f224d7ecac8b9b4a15a20218679c2
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 13:40:27 +0200

    Exercise 22

---
+++ homework/01/e22.py
@@ -0,0 +1,11 @@
+print("Anna luku")
+luku = int(input())
+if luku >= 0:
+    while luku >= 0 :
+        print(luku)
+        luku = luku - 1
+
+elif luku <= 0:
+    while luku <= 0:
+        print(luku)
+        luku = luku + 1
