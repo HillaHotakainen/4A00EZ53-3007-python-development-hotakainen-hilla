1ee0a7cde78afd5015120fa5728a4fbfab9e3280
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 13:09:04 +0200

    Exercise 21

---
+++ homework/01/e21.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+print("Anna neliön korkeus")
+korkeus = int( input() )
+print("Anna neliön leveys")
+leveys = int( input() )
+rivi = 0
+while rivi < korkeus:
+    sarake = 0
+    while sarake < leveys:
+        # Tulosta "X" ilman enter painallusta
+        # Huom! tämä vaatii python3 - tulkin joka asetettu päälle rivillä 1
+        print("X", end='')
+        sarake = sarake + 1
+    
+    # Tulosta enter
+    print()
+    rivi = rivi + 1
