9dfc3091d56391ccf666858358fa3ac4b2cb719b
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-23 11:46:17 +0200

    Exercise 16

---
+++ homework/01/e16.py
@@ -0,0 +1,21 @@
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+i = 10
+while i > 0:
+    print(i)
+    i = i - 1
+
+print("Anna luku")
+N = int(input())
+i = 1
+while i <= N:
+    print(i)
+    i = i + 1
