b7f465b7f83c33e617dc14e759a5376b71980558
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-28 14:37:03 +0200

    Exercise four

---
+++ homework/02/e04.py
@@ -0,0 +1,36 @@
+luku_hasu = str(5)
+hasu_luku = str(5.5)
+hupsu_sana = "hello"
+booli_bileet = str(True)
+
+print(luku_hasu, type(luku_hasu))
+print(hasu_luku, type(hasu_luku))
+print(hupsu_sana, type(hupsu_sana))
+print(booli_bileet, type(booli_bileet))
+
+print(luku_hasu + hasu_luku)
+print(float(luku_hasu)+float(hasu_luku))
+
+floatti=6.23
+print(int(floatti))
+
+floatti2=8.3456
+print(round(floatti2))
+
+int=float(25)
+print(int)
+
+int1=2
+int2=5
+tulos1=int1 + int2
+print(tulos1, type(tulos1))
+
+int3=6
+floatti3 = 5.4
+tulos2 = int3 + floatti3
+print(tulos2, type(tulos2))
+
+int4 = 7
+int5 = 2
+jako = int4 / int5
+print(jako, type(jako))
