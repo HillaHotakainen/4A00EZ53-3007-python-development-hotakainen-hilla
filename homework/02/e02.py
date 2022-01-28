521aa68360c9d2f2b48e62d456a81979f4f0ca72
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-28 13:07:55 +0200

    Second exercise

--- homework/02/e02.py
+++ homework/02/e02.py
@@ -1,1 +1,13 @@
-print("This is exercise 02")
+from pickle import TRUE
+from winreg import HKEY_LOCAL_MACHINE
+
+
+luku_hasu = 5
+hasu_luku = 5.5
+hupsu_sana = "hello"
+booli_bileet = True
+
+print(luku_hasu, type(luku_hasu))
+print(hasu_luku, type(hasu_luku))
+print(hupsu_sana, type(hupsu_sana))
+print(booli_bileet, type(booli_bileet))
