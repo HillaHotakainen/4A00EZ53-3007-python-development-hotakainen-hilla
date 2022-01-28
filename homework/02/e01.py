1c654cb790916f38371e2aeea3993ca9830117f7
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-01-28 12:58:36 +0200

    First exercise

--- homework/02/e01.py
+++ homework/02/e01.py
@@ -1,1 +1,21 @@
-print("This is exercise 01")
+
+a = "hello"
+b = "world"
+
+print(a) #Printtaa hello
+print(a, b) #Printtaa hello world
+print(a, b, sep=":") #Printtaa hello:world
+print(a, b, sep=":", end=";") #Printtaa hello:world;
+print("\n") #Printtaa rivin vaihdon
+
+#Printtaa: 
+#hello
+#world
+print(a, "\n")
+print(b)
+
+"""Printtaa helloworld"""
+print(a, end="")
+print(b)
+
+help(print)
