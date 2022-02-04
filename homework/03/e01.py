439c75cdafcb45738a0ed24784403cf265284d8b
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-02-04 14:10:51 +0200

    Exercise number one

--- homework/03/e01.py
+++ homework/03/e01.py
@@ -1,1 +1,11 @@
-print("This is exercise 01")
+for x in range (0, 10):
+    print(f" {x},", end = " ")
+print(x +1)
+
+for x in range (0, 10, 2):
+    print(f" {x},", end = " ")
+print(x +2)
+
+for x in range (10, 0, -1):
+    print(f" {x},", end = " ")
+print(x -1)
