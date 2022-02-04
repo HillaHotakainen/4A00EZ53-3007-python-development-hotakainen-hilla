5c03b4eb7d32cc327ec9aa40adf6f3c440456b67
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-02-04 14:12:41 +0200

    Fiexer first a bit

--- homework/03/e01.py
+++ homework/03/e01.py
@@ -1,11 +1,11 @@
 for x in range (0, 10):
-    print(f" {x},", end = " ")
+    print(f" {x},", end = "")
 print(x +1)
 
 for x in range (0, 10, 2):
-    print(f" {x},", end = " ")
+    print(f" {x},", end = "")
 print(x +2)
 
 for x in range (10, 0, -1):
-    print(f" {x},", end = " ")
+    print(f" {x},", end = "")
 print(x -1)
