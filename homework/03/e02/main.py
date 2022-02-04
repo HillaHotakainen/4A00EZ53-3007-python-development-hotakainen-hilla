e73779a1efb07fd85bf33abe4689995a27ff9e34
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-02-04 15:35:12 +0200

    Exercise two

---
+++ homework/03/e02/main.py
@@ -0,0 +1,8 @@
+from string_helper import is_name
+
+print("Your name:")
+your_name = input()
+if is_name(your_name) == True:
+    print("Hello" , your_name, end= "!")
+else:
+    print("You did not give a proper name.")
