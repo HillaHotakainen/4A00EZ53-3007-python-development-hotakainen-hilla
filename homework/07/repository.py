7dc6ff89053911df61a5cc75c9e76fdfe1aa3e09
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-03-11 14:27:01 +0200

    exercise four

--- homework/07/repository.py
+++ homework/07/repository.py
@@ -2,4 +2,13 @@ def read_database():
     f = open("database.txt", "r")
     return f.read()
 
+def save_to_database(fname, lname):
+    f = open("database.txt", "r")
+    for x in f:
+        last_line = (x)
+    id_number = int(last_line[0])+1
+    result = open("database.txt", "a")
+    result.write(f"\n{id_number},{fname},{lname}")
+
+save_to_database("James", "Bond")
 print(read_database())
