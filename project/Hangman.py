f16a10c4d949c0363d9dd6bbc56576175fa47ef5
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-04-21 17:11:32 +0300

    Base and asking name

--- project/Hangman.py
+++ project/Hangman.py
@@ -1,4 +1,4 @@
-pics = ["  _____\n  |   |\n      |\n      |\n      |\n      |\n¯¯¯¯¯¯¯", 
+pic = ["  _____\n  |   |\n      |\n      |\n      |\n      |\n¯¯¯¯¯¯¯", 
 
         "  _____\n  |   |\n  O   |\n      |\n      |\n      |\n¯¯¯¯¯¯¯",
 
@@ -12,4 +12,18 @@ pics = ["  _____\n  |   |\n      |\n      |\n      |\n      |\n¯¯¯¯¯¯¯",
 
         "  _____\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n¯¯¯¯¯¯¯"]
 
-print(pics[1])
+guesses = 0
+player_name = ""
+word = ["apina", "kissa"] #placeholder
+guess_word = word[1] #placeholder
+
+#defs here
+
+print("Hello!", "This a basic game of hangman")
+while len(player_name) < 1:
+        print("Please give me your name")
+        name = input()
+        if len(name) > 1:
+                player_name = name
+
+print(name, player_name)
