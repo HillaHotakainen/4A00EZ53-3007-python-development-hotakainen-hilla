05f0c0889e0518540a226613d51209bff8787c63
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-04-25 15:03:32 +0300

    made methods for pulling score and sorting it based on time

--- project/Hangman.py
+++ project/Hangman.py
@@ -20,6 +20,7 @@ player_name = ""
 f = open("wordlist.txt", "r")
 word = f.readlines()
 guess_word = random.choice(word)[:-1]
+f.close()
 guessed_letters = []
 print("Hello!", "This is a basic game of hangman")
 while len(player_name) < 1:
@@ -27,6 +28,23 @@ while len(player_name) < 1:
         name = input()
         if len(name) > 1:
                 player_name = name
+def get_time(elem):
+        return elem[1]
+
+def pull_scores():
+        f = open("highscores.txt", "r")
+        scores = f.read()
+        f.close()
+        return scores
+
+def scores_to_2D():
+        pull = pull_scores()
+        twoD = []
+        lines = pull.strip().split("\n")
+        for line in lines:
+                twoD.append(line.split(","))
+        twoD.sort(key=get_time)
+        return twoD
 
 def play_the_game(player_name):
         print("Okay", player_name.capitalize(), "Let's start!")
@@ -42,6 +60,10 @@ def play_the_game(player_name):
                         elapsed_time = (time.perf_counter_ns() - t) / 1000000000
                         print("That was the correct word, you won!")
                         print("Your time was", elapsed_time, "seconds")
+                        if guess_word == "bunny":
+                                sorted = scores_to_2D()
+                                if float(sorted[0][1]) < elapsed_time and float(sorted[1][1]) < elapsed_time and float(sorted[2][1]) < elapsed_time:
+                                        print("jee")
                 elif number_of_guesses < 5:
                         number_of_guesses = number_of_guesses + 1
                         guessed_letters.append(guess.lower())
@@ -50,7 +72,6 @@ def play_the_game(player_name):
                                         print(letter, end= " ")
                                 else:
                                         print("_", end=" ")
-                        print("Wrong one!")
                         print("Guesses left:", 6 - number_of_guesses)
                         print(pic[number_of_guesses])
                 else:
