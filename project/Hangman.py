54928c7e6cfb549607ace376aaf8e31bdfbd70cf
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2022-04-25 11:21:54 +0300

    random word choosing last push was missing

--- project/Hangman.py
+++ project/Hangman.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 pic = ["  _____\n  |   |\n      |\n      |\n      |\n      |\n¯¯¯¯¯¯¯", 
 
@@ -16,10 +17,11 @@ pic = ["  _____\n  |   |\n      |\n      |\n      |\n      |\n¯¯¯¯¯¯¯",
 
 guesses = 0
 player_name = ""
-word = ["apina", "kissa"] #placeholder
-guess_word = word[1] #placeholder
+f = open("wordlist.txt", "r")
+word = f.readlines()
+guess_word = random.choice(word)[:-1]
+print(guess_word)
 guessed_letters = []
-
 print("Hello!", "This is a basic game of hangman")
 while len(player_name) < 1:
         print("Please give me your name")
@@ -37,25 +39,24 @@ def play_the_game(player_name):
         while guess != guess_word and number_of_guesses < 6:
                 print("What is your guess?")
                 guess = input()
-                guessed_letters.append(guess.lower())
-                for letter in guess_word:
-                        if letter.lower() in guessed_letters:
-                                print(letter, end= " ")
-                        else:
-                                print("_", end=" ")
                 if guess == guess_word:
                         elapsed_time = (time.perf_counter_ns() - t) / 1000000000
-                        print("You won!")
+                        print("That was the correct word, you won!")
                         print("Your time was", elapsed_time, "seconds")
                 elif number_of_guesses < 5:
                         number_of_guesses = number_of_guesses + 1
+                        guessed_letters.append(guess.lower())
+                        for letter in guess_word:
+                                if letter.lower() in guessed_letters:
+                                        print(letter, end= " ")
+                                else:
+                                        print("_", end=" ")
                         print("Wrong one!")
                         print("Guesses left:", 6 - number_of_guesses)
                         print(pic[number_of_guesses])
                 else:
-                        elapsed_time = (time.perf_counter_ns() - t) / 1000000000
                         print("Game over!")
-                        print("Your time was", elapsed_time, "seconds")
+                        print("the correct word was", guess_word)
                         number_of_guesses = number_of_guesses + 1
                         print(pic[number_of_guesses])
 
